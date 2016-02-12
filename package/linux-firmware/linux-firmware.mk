3076eed7706d8c1e3b7a503995b80db3f2c401ae
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2016-02-12 18:01:58 +0300

    Added ath3k firmware

--- package/linux-firmware/linux-firmware.mk
+++ package/linux-firmware/linux-firmware.mk
@@ -8,6 +8,10 @@ LINUX_FIRMWARE_VERSION = 3161bfa479d5e9ed4f46b57df9bcecbbc4f8eb3c
 LINUX_FIRMWARE_SITE = http://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 LINUX_FIRMWARE_SITE_METHOD = git
 
+ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_ATHEROS_3011),y)
+LINUX_FIRMWARE_FILES += ath3k-1.fw
+endif
+
 # Intel SST DSP
 ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_INTEL_SST_DSP),y)
 LINUX_FIRMWARE_FILES += intel/fw_sst_0f28.bin-48kHz_i2s_master
